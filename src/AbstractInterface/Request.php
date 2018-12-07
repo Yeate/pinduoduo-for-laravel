0e953a49bdae706c8225402f7a716010d739393c
yzc <569508391@qq.com>
2018-12-07 17:27:26 +0800

    支持cms商城

--- src/AbstractInterface/Request.php
+++ src/AbstractInterface/Request.php
@@ -24,4 +24,27 @@ class Request
     private $version;
 
     private $sign;
+    
+    function __call(string $function_name, $arguments=[])
+    {
+        $action = '';
+        $param = lcfirst(preg_replace_callback('/get|set/',function($match)use(&$action){
+            $action = $match;
+        },'getCustomParameters'));
+        if(count($action)==1){
+            switch ($action) {
+                case 'get':
+                    if(isset($this->$param)){
+                        return $this->$param;
+                    }
+                    break;
+                case 'set':
+                    if(isset($this->$param) && isset($arguments['0'])){
+                        $this->$param=$arguments['0'];
+                    }
+                    break;
+            }
+        }
+        throw new \Exception($function_name." can't find");
+    }
 }
